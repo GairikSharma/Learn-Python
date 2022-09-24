2d5d8855155dc0be7b219474d53af95d100f3afb
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-09-25 01:59:30 +0530

    commit

---
+++ strings/string_modify.py
@@ -0,0 +1,11 @@
+from dataclasses import replace
+
+
+a=" helloWorld  "
+b="helloserver"
+print(a.upper()) # whole string into uppercase
+print(a.lower()) # whole string into lowercase
+print(a.strip()) # removes all whitespaces
+#replacig string
+print(a.replace("l","g"))
+print(b.split())
