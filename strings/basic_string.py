75ea4226cd5467ebd25d278f1623fcb16fa9077d
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-09-25 01:30:20 +0530

    commit

---
+++ strings/basic_string.py
@@ -0,0 +1,12 @@
+print('Hello World')
+#or
+x='Hello World'
+print(x)
+
+#multiline strings -> You can assign a multiline string to a variable by using three quotes
+a="""Hello world, 
+my name is gairik,
+I am a frontend developer
+"""
+print(a)
+
---
+++ strings/string_slicing.py
@@ -0,0 +1,14 @@
+s='HelloWorld'
+print(s[1:5]) # -> ello : it starts from index 1 and return upto index 4
+
+#slicing from start 
+a='Helloserver'
+print(a[:3]) # -> Hel : as here we have not specify the starting index, i.e it will starts from index 1
+
+#slicing to end
+x='HelloGairik'
+print(x[2:]) # -> lloGairik : as here we have not specify the ending index, i.e it will ends in last index
+
+#negative indexing
+p="Hello, Server"
+print(p[-5:-2])
