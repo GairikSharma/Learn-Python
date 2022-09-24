17eb8b30b8a9f6adf412fb159282546dbbf8082f
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-09-25 02:21:25 +0530

    Commit

---
+++ strings/string_format.py
@@ -0,0 +1,4 @@
+x="My name is {}, I'm a {}"
+name="Gairik Sharma"
+skill="Developer"
+print(x.format(name, skill))
--- strings/string_modify.py
+++ strings/string_modify.py
@@ -9,3 +9,8 @@ print(a.strip()) # removes all whitespaces
 #replacig string
 print(a.replace("l","g"))
 print(b.split())
+
+#concatinate
+x='Hey all'
+y='myself gairik'
+print(x+' '+y)
