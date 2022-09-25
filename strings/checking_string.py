398784eac72cf0d194491094c86347baf8182264
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-09-25 18:32:03 +0530

    Commit string

---
+++ strings/checking_string.py
@@ -0,0 +1,7 @@
+x="Hello, world myself , gairik"
+print("gairik" in x) # -> checking if gairik is present in the string or not
+y="myself gairik, a ece student"
+if "ece" in y:  # using in if block
+    print('True')
+if "sharma" not in y:
+    print('False')
---
+++ strings/string_as_array.py
@@ -0,0 +1,8 @@
+x="Hello, World"
+print(x[1])
+
+#looping throug a string , or travarsing all the values of a string using loop
+# for i in range(len(x)):
+#     print(i)
+for i in x:
+    print(i)
